b85c5aead88bf09d25d1114fd734eda6cb22a956
evangel <evangeldaping@gmail.com>
2021-07-26 15:57:46 +0800

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <div className="app" style={{backgroundImage: "url(./ques1.png)"}}>
+        <Header/>
+      </div>
+    </BrowserRouter>
   );
 }
 
