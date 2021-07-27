f073637201d85e61de26ff40f6cce1848ef4dfe1
evangel <evangeldaping@gmail.com>
2021-07-27 12:23:59 +0800

    Result Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
 import axios from "axios";
-import { useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Home from './Pages/Home/Home';
-import Quiz from './Pages/Quiz/Quiz';
-import Result from './Pages/Result/Result';
+import { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
 
 function App() {
   const [questions, setQuestions] = useState();
@@ -16,29 +15,26 @@ function App() {
 
   const fetchQuestions = async (category = "", difficulty = "") => {
     const { data } = await axios.get(
-      `https://opentdb.com/api.php?amount=10${
-        category && `&category=${category}`
+      `https://opentdb.com/api.php?amount=10${category && `&category=${category}`
       }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
     );
 
     setQuestions(data.results);
   };
-  
+
   return (
     <BrowserRouter>
-      <div className="app" style={{backgroundImage: "url(./ques1.png)"}}>
-        <Header/>
-
+      <div className="app" style={{ backgroundImage: 'url("/ques1.png")' }}>
+        <Header />
         <Switch>
-          <Route path='/' exact>
+          <Route path="/" exact>
             <Home
               name={name}
               setName={setName}
               fetchQuestions={fetchQuestions}
             />
           </Route>
-
-          <Route path='/quiz' exact>
+          <Route path="/quiz">
             <Quiz
               name={name}
               questions={questions}
@@ -47,13 +43,12 @@ function App() {
               setQuestions={setQuestions}
             />
           </Route>
-
-          <Route path='/result' exact>
-            <Result />
+          <Route path="/result">
+            <Result name={name} score={score} />
           </Route>
         </Switch>
       </div>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
